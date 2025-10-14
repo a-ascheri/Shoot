9ab113ca8861bf557acfd4fb53a05e461dd9d583
Agustin Ascheri <ascheriagustin@gmail.com>
2025-10-14 15:37:29 -0300

    Estructura para modularizar - PlayerPowerBar modularizado updated

--- Assets/Scripts/PlayerPowerBar.cs
+++ Assets/Scripts/PlayerPowerBar.cs
@@ -23,7 +23,7 @@ public class PlayerPowerBar : MonoBehaviour
         Color endColor;
         if (force < 10f)
             endColor = Color.green;
-        else if (force < 20f)
+        else if (force < 95f)
             endColor = Color.yellow;
         else
             endColor = Color.red;
@@ -31,17 +31,19 @@ public class PlayerPowerBar : MonoBehaviour
         Gradient gradient = new Gradient();
         gradient.SetKeys(
             new GradientColorKey[] {
-                new GradientColorKey(Color.green, 0f),
-                new GradientColorKey(endColor, forceRatio),
-                new GradientColorKey(endColor, 0.8f),
-                new GradientColorKey(Color.clear, 0f)
+                new GradientColorKey(Color.green, 0f),                                // Verde al inicio de la barra (0%)
+                new GradientColorKey(Color.green, Mathf.Clamp01(forceRatio * 0.3f)),  // Amarillo en el punto medio según la fuerza
+                new GradientColorKey(Color.yellow, Mathf.Clamp01(forceRatio * 0.6f)),
+                new GradientColorKey(Color.red, Mathf.Clamp01(forceRatio * 0.99f)),
+                new GradientColorKey(Color.red, forceRatio),                          // Rojo en el punto de fuerza actual
+                new GradientColorKey(Color.clear, 1f)                                 // Transparente al final (100%)
             },
             new GradientAlphaKey[] {
-                new GradientAlphaKey(1f, 0f),
-                new GradientAlphaKey(1f, 0f),
-                new GradientAlphaKey(1f, 0f),
-                new GradientAlphaKey(0f, 1f)
-            }
+                new GradientAlphaKey(1f, 0f),       // 1) En el inicio de la barra (0%), la barra es opaca (alpha = 1)
+                new GradientAlphaKey(0.75f, 0.5f),  // 2) En el mismo inicio (0%), sigue siendo opaca (repetido, no aporta diferencia)
+                new GradientAlphaKey(0.5f, 0.75f),  // 3) En la mitad de la barra (50%), sigue siendo opaca (alpha = 1)
+                new GradientAlphaKey(0f, 1f)        // 4) En el extremo final (100%), la barra es completamente transparente (alpha = 0)
+        }
         );
         line.colorGradient = gradient;
 
